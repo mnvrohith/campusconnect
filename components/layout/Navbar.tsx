7db6d017fe9c5bc65b4c3ea89e17f0ac233f7592
mnvrohith <mnvrohith@gmail.com>
2026-05-21 07:46:28 +0530

    fix: remove afterSignOutUrl prop from UserButton in Navbar component

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -88,7 +88,7 @@ export default function Navbar() {
           ) : (
             <>
               {/* User Avatar */}
-              <UserButton afterSignOutUrl="/" />
+              <UserButton />
             </>
           )}
 
